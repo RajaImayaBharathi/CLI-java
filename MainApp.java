b3fea72accd8c3556909c4390de78517dad520fd
RajaImayaBharathi <rajaimayabharathi@gmail.com>
2025-02-17 16:13:34 +0530

    file structure

---
+++ MainApp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import controller.*;
+
+public class MainApp {
+    static Scanner scan=new Scanner(System.in);
+    static Auth auth=new Auth();
+    //static User loggedInUser=null;
+    public static void main(String[] args) {
+        
+        scan.close();
+    }
+}
